a717b6d9faefbe9985077c92f8d9eaaab01f1d0d
fdbcr <40166948+fdbcr@users.noreply.github.com>
2018-06-12 13:12:50 +0200

    Upload Files
    Upload Files

---
+++ Prueba.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthwindMvc5
+{
+    public class Prueba
+    {
+        public string Nombre { get; set; }
+
+        private int edad;
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+
+            set
+            {
+                edad = value;
+            }
+        }
+
+        public double Pruebas { get; set; }
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+        public void Metodo()
+        {
+
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            try
+            {
+
+            }
+            finally
+            {
+
+            }
+
+            if (true)
+            {
+
+            }
+
+        }
+
+    }
+}
